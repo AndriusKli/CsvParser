73d3fd77164137a1442313886da41900cbf72a8a
Andrius <andriuskliunka@gmail.com>
2024-05-23 17:21:01 +0300

    Handle scenarios when there are equally popular items or days with equally high revenue

--- src/main/java/org/andriusk/service/TransactionDetailsService.java
+++ src/main/java/org/andriusk/service/TransactionDetailsService.java
@@ -5,15 +5,20 @@ import org.andriusk.dto.TransactionDetails;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
+import static java.util.stream.Collectors.toSet;
 
 public class TransactionDetailsService {
     public static void generateReport(List<TransactionDetails> transactionDetails) {
         System.out.println("Total Revenue: " + calculateTotalRevenue(transactionDetails));
         System.out.println("Unique Customers: " + calculateUniqueCustomers(transactionDetails));
-        System.out.println("Most Popular Item: " + findMostPopularProduct(transactionDetails));
-        System.out.println("Date with Highest Revenue: " + findMostProfitableDate(transactionDetails));
+        System.out.println("Most Popular Item(s): " + findMostPopularProduct(transactionDetails));
+        System.out.println("Date(s) with Highest Revenue: " + findMostProfitableDate(transactionDetails));
     }
     public static Double calculateTotalRevenue(List<TransactionDetails> transactionDetails) {
         return transactionDetails.stream()
@@ -28,26 +33,31 @@ public class TransactionDetailsService {
                 .size();
     }
 
-    public static String findMostPopularProduct(List<TransactionDetails> transactionDetails) {
+    public static Set<String> findMostPopularProduct(List<TransactionDetails> transactionDetails) {
         return transactionDetails.stream()
                 .map(TransactionDetails::getItemId)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
-                // This doesn't handle scenarios where there are multiple equally-popular products, but I'm a bit
-                // short on time to properly implement it.
-                .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey).orElseThrow();
+                // Messy, but in case there are multiple products with the same popularity
+                .collect(groupingBy(Map.Entry::getValue, mapping(Map.Entry::getKey, toSet())))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue).orElseThrow();
     }
 
-    public static LocalDate findMostProfitableDate(List<TransactionDetails> transactionDetails) {
+    public static Set<LocalDate> findMostProfitableDate(List<TransactionDetails> transactionDetails) {
         return transactionDetails.stream()
                 .collect(Collectors.groupingBy(TransactionDetails::getTransactionDate,
                         Collectors.summingDouble(TransactionDetails::getItemPrice)))
                 .entrySet()
                 .stream()
                 // Ditto
-                .max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey).orElseThrow();
+                .collect(groupingBy(Map.Entry::getValue, mapping(Map.Entry::getKey, toSet())))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByKey())
+                .map(Map.Entry::getValue).orElseThrow();
     }
 }
--- src/test/java/org/andriusk/service/TransactionDetailsServiceTest.java
+++ src/test/java/org/andriusk/service/TransactionDetailsServiceTest.java
@@ -8,6 +8,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TransactionDetailsServiceTest {
 
@@ -41,29 +42,40 @@ class TransactionDetailsServiceTest {
         TransactionDetails details1 = new TransactionDetails();
         TransactionDetails details2 = new TransactionDetails();
         TransactionDetails details3 = new TransactionDetails();
+        TransactionDetails details4 = new TransactionDetails();
+        TransactionDetails details5 = new TransactionDetails();
 
         details1.setItemId("someItem1");
         details2.setItemId("someItem2");
         details3.setItemId("someItem1");
+        details4.setItemId("someItem2");
+        details5.setItemId("someItem55");
 
-        assertEquals("someItem1", TransactionDetailsService.findMostPopularProduct(List.of(details1, details2, details3)));
+        List<String> expectedValues = List.of("someItem1", "someItem2");
+        assertTrue(TransactionDetailsService.findMostPopularProduct(List.of(details1, details2, details3, details4, details5)).containsAll(expectedValues));
     }
 
     @Test
     void findMostProfitableDate() {
+        LocalDate today = LocalDate.now();
+        LocalDate tomorrow = LocalDate.now().plus(1, ChronoUnit.DAYS);
+        LocalDate yesterday = LocalDate.now().minus(1, ChronoUnit.DAYS);
         TransactionDetails details1 = new TransactionDetails();
         TransactionDetails details2 = new TransactionDetails();
         TransactionDetails details3 = new TransactionDetails();
+        TransactionDetails details4 = new TransactionDetails();
 
 
         details1.setItemPrice(1.01d);
-        details1.setTransactionDate(LocalDate.now());
+        details1.setTransactionDate(today);
         details2.setItemPrice(5.04d);
-        details2.setTransactionDate(LocalDate.now().plus(1, ChronoUnit.DAYS));
+        details2.setTransactionDate(tomorrow);
         details3.setItemPrice(7.00d);
-        details3.setTransactionDate(LocalDate.now());
+        details3.setTransactionDate(today);
+        details4.setItemPrice(8.01d);
+        details4.setTransactionDate(yesterday);
 
-        assertEquals(LocalDate.now(), TransactionDetailsService.findMostProfitableDate(List.of(details1, details2, details3)));
-
+        List<LocalDate> expectedDates = List.of(today, yesterday);
+        assertTrue(TransactionDetailsService.findMostProfitableDate(List.of(details1, details2, details3, details4)).containsAll(expectedDates));
     }
 }
